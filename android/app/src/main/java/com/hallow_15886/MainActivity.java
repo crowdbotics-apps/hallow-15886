0c4286cbb926e1c72d1f3cbd1d04ad4393dbc191
Crowdbotics <team@crowdbotics.com>
2020-04-15 19:07:53 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/hallow_15886/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hallow_15886;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "hallow_15886";
+    }
+}
